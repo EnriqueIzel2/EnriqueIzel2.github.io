dccec09aaa5c0983915cc46c07eda6a323319333
EnriqueIzel2 <eleaoizel@gmail.com>
2021-10-29 21:26:07 -0400

    feat: change class selector to data attribute and direction calculation
    reverse direction of card movement

--- js/components/main-carousel.js
+++ js/components/main-carousel.js
@@ -1,14 +1,14 @@
-const btnRight = document.querySelector('.button-arrow.--right');
-const btnLeft = document.querySelector('.button-arrow.--left');
-const elements = document.querySelector('.elements');
+const btnRight = document.querySelector("[btnArrowRight]");
+const btnLeft = document.querySelector("[btnArrowLeft]");
+const elements = document.querySelector(".elements");
 let pixels = 50;
 
-btnRight.addEventListener('click', () => {
-  pixels += 50;
+btnRight.addEventListener("click", () => {
+  pixels -= 50;
   elements.style.transform = `translateX(${pixels}px)`;
 });
 
-btnLeft.addEventListener('click', () => {
-  pixels -= 50;
+btnLeft.addEventListener("click", () => {
+  pixels += 50;
   elements.style.transform = `translateX(${pixels}px)`;
 });
